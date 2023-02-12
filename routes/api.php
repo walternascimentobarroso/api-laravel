35bb05c44c0e21d584d00d100e7bba9db4807aec
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-02-11 22:15:42 -0400

    ✨ NEW FEATURE: Adjust in group routes

--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,22 @@ use App\Http\Controllers\TelegramController;
 use App\Http\Controllers\QuestionController;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('options', OptionController::class);
     Route::apiResource('questions', QuestionController::class);
-    Route::get('telegram/me', [TelegramController::class, 'me'])->name('telegram.me');
-    Route::get('telegram/groups', [TelegramController::class, 'groups'])->name('telegram.groups');
-    Route::get('telegram/sendMessage', [TelegramController::class, 'sendMessage'])->name('telegram.sendMessage');
+
+    Route::controller(TelegramController::class)->prefix('telegram')->group(function () {
+        Route::get('me', 'me')->name('telegram.me');
+        Route::get('groups', 'groups')->name('telegram.groups');
+        Route::get('sendMessage', 'sendMessage')->name('telegram.sendMessage');
+    });
 });
 
 
-Route::get('/me', [AuthController::class, 'me'])->name('auth.me')->middleware('auth:sanctum');
-Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
-Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('me', 'me')->name('auth.me')->middleware('auth:sanctum');
+    Route::post('login', 'login')->name('auth.login');
+    Route::post('logout', 'logout')->name('auth.logout')->middleware('auth:sanctum');
+    Route::post('register', 'register')->name('auth.register');
+});
